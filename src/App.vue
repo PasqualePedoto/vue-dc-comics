f4a68e5a70d30224da2632b6dbb8bff46a6e6aba
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-07-22 14:36:26 +0200

    Add three components

--- src/App.vue
+++ src/App.vue
@@ -10,30 +10,22 @@ farvi venire qualche grattacapo! Cercate di trovare una soluzione da veri progra
 proprio in caso di emergenza, aprite un ticket! -->
 
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div></div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import TheHeader from "./components/TheHeader.vue";
+import TheMain from "./components/TheMain.vue";
+import TheFooter from "./components/TheFooter.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    TheHeader,
+    TheMain,
+    TheFooter,
   },
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"></style>
