934f23405ab1d2d6e334ba48f206eed1c0595ef3
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-07-22 18:12:59 +0200

    added main component

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -1,5 +1,9 @@
 <template>
-  <div></div>
+  <main>
+    <div class="container">
+      <h2>Content is here</h2>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -8,4 +12,14 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+main {
+  padding: 50px;
+  background-color: black;
+
+  h2 {
+    font-weight: bold;
+    color: white;
+  }
+}
+</style>
