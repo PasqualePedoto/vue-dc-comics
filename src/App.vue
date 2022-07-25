09b54b9e78e65f7c390379aa5b3ce1226dedce96
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-07-25 16:52:59 +0200

    Aggiunto la cartella sass in assets - Progetto completato

--- src/App.vue
+++ src/App.vue
@@ -169,37 +169,5 @@ export default {
 </script>
 
 <style lang="scss">
-// GENERICS
-
-* {
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-}
-
-img {
-  max-width: 100%;
-  height: 100%;
-}
-
-a {
-  text-decoration: none;
-}
-
-ul > li {
-  list-style-type: none;
-}
-
-.container {
-  max-width: 1200px;
-  margin: 0 auto;
-  height: auto;
-}
-
-.set-display {
-  display: flex;
-  flex-direction: column;
-
-  position: relative;
-}
+@import "./assets/sass/style.scss";
 </style>
